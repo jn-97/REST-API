1fad7b3ad26e0adec95068a3cea2f536817f9640
jn-97 <kjo5395@gmail.com>
2024-04-23 14:28:54 +0900

    add: serializer.py + api.py

---
+++ student/api.py
@@ -0,0 +1,11 @@
+from rest_framework import APIView
+from rest_framework.response import Response
+from .serializers import *
+
+class StudentList(APIView):
+
+    def get(self, request):
+
+        model = Student.objects.all() 
+        serializer = StudentSerializer(model, many=True) 
+        return Response(serializer.date)
