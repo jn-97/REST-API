f9f3ca02f8894824014d12b330a3810a4dc82af4
jn-97 <kjo5395@gmail.com>
2024-04-23 17:09:55 +0900

    add: token

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 import requests # requests 라이브러리 사용
 
-url = 'http://127.0.0.1:8000/api/student_list'
+url = 'http://127.0.0.1:8000/api/auth/'
 
-response = requests.get(url)
+response = requests.post(url, data = {'username': 'admin', 'password': '1234'})
 
 print(response.text)
+
+myToken = response.text
